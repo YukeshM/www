b56782ef96e71848178cb820f3f7f70c15210908
Yukesh M <mlyukesh@gmail.com>
2020-12-25 22:06:12 +0530

    .

--- for_loop.php
+++ for_loop.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <?php
-    $tables = array(4,8,12,16,20,24,28,32,36 );
+    $tables = array(4,8,12,16,20,24,28,32,36,40 );
     for($i = 0; $i < count($tables); $i++){
       echo "$tables[$i] <br>";
     }
